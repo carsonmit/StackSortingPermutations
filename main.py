c62a7241829552f9fd184dcad08bb95742a2b368
carsonmit <carsonm6719@gmail.com>
2022-04-20 18:43:32 -0700

    Update

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from AscentsTables import ascentSSTable,startsWithSSTable
 
+'''These tables provide two ways of partitioning permutations of S_n based on their behavior, one (likely more strong)
+based on the number of ascents in a perm and the other on the number the perm starts with. The two functions are labeled 
+accordingly and their only input is n, the desired size of S_n. Below the definition, you'' find functions whose
+ input you can swap out for whatever you like.'''
+
+
 def ascentTable(n):
     print('''The columns represent the number of ascents a given permutation in S_n has. These are the Eulerian numbers
     and the kth column will contain A(n,k) total permutations. Furthermore, each of these A(n,k) are partitioned into n 
@@ -8,6 +14,7 @@ def ascentTable(n):
     print(ascentSSTable(n))
     return None
 
+
 def swTable(n):
     print('''The columns represent the number a given permutation in S_n starts with. There are n!/n = (n-1)! of these
     permutations in each column. Furthermore, each of these (n-1)! are partitioned into n rows depending on if they are 
@@ -15,7 +22,6 @@ def swTable(n):
     print('Starting Number:')
     print(startsWithSSTable(n))
     return None
-
 
 
 '''Modify this code here! All you need to do is change the input n to the size you want.'''
